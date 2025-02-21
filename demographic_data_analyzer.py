df52ed9ef7021136831ef617d37824159933d754
Adam Kubica <kubica.adam@gmail.com>
2025-02-21 22:52:47 +0000

    step5

--- demographic_data_analyzer.py
+++ demographic_data_analyzer.py
@@ -9,29 +9,34 @@ def calculate_demographic_data(print_data=True):
     race_count = df.groupby(["race"]).size()
 
     # What is the average age of men?
-    average_age_men = df.loc[df["sex"] == "Male"]["age"].mean()
+    average_age_men = df.loc[df["sex"] == "Male"]["age"].mean().round(1)
 
     # What is the percentage of people who have a Bachelor's degree?
-    percentage_bachelors = None
+    percentage_bachelors = round(df.loc[df["education"] == "Bachelors"].shape[0] / df["education"].shape[0] * 100.0, 1)
 
     # What percentage of people with advanced education (`Bachelors`, `Masters`, or `Doctorate`) make more than 50K?
     # What percentage of people without advanced education make more than 50K?
 
+    he_df = df.loc[(df["education"].isin(["Bachelors", "Masters", "Doctorate"]))]
+    le_df = df.loc[~(df["education"].isin(["Bachelors", "Masters", "Doctorate"]))]
+
     # with and without `Bachelors`, `Masters`, or `Doctorate`
-    higher_education = None
-    lower_education = None
+    higher_education = round(he_df.loc[he_df["salary"] == "<50K"].shape[0] / he_df.shape[0] * 100, 1)
+    lower_education = round(le_df.loc[le_df["salary"] == "<50K"].shape[0] / le_df.shape[0] * 100, 1)
 
     # percentage with salary >50K
-    higher_education_rich = None
-    lower_education_rich = None
+    higher_education_rich = round(he_df.loc[he_df["salary"] == ">50K"].shape[0] / he_df.shape[0] * 100, 1)
+    lower_education_rich = round(le_df.loc[le_df["salary"] == ">50K"].shape[0] / le_df.shape[0] * 100, 1)
 
     # What is the minimum number of hours a person works per week (hours-per-week feature)?
-    min_work_hours = None
+    min_work_hours = df["hours-per-week"].min()
 
     # What percentage of the people who work the minimum number of hours per week have a salary of >50K?
-    num_min_workers = None
+    mh_df = df.loc[df["hours-per-week"] == min_work_hours]
 
-    rich_percentage = None
+    num_min_workers = mh_df.shape[0]
+
+    rich_percentage = round(mh_df.loc[mh_df["salary"] == ">50K"].shape[0] / num_min_workers * 100, 1)
 
     # What country has the highest percentage of people that earn >50K?
     highest_earning_country = None
